a27e45436d038ead6953e1cd5494bffb3b277936
Aaron Bergemann <aaron.bergemann@snapengage.com>
2016-04-21 14:04:26 +0200

    Update jenv config to include maven support

--- jenv/install.sh
+++ jenv/install.sh
@@ -14,8 +14,12 @@ echo 'eval "$(jenv init -)"' >> ~/.zshrc
 # Add JDKs
 jenv add /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home
 jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_74.jdk/Contents/Home
+jenv rehash
 
 # Use JDK7 as global default
 jenv global oracle64-1.7.0.80
+
+# Make maven aware of jenv
+jenv enable-plugin maven
 
 exit 0
