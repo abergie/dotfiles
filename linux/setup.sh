e846c77c95f93ee721756b17d9cbcf6ce6ed4f0b
bergie <aaron.bergemann@gmail.com>
2020-03-27 00:54:21 -0600

    Add Linux configs

---
+++ linux/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt install git zsh curl xclip
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
