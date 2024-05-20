c1c2ac282ffb506d7e3b2cf9c913dcecb4eda33d
Aaron Bergemann <aaron.bergemann@concentrix.com>
2024-05-20 11:10:36 -0600

    more updates

--- zsh/merge-history.zsh
+++
@@ -1,7 +0,0 @@
-# # Use following commands to merge multiple .zsh_history files
-
-# builtin fc -R -I "$hist_file"
-# builtin fc -R -I "$another_hist_file"
-
-# # write the loaded history to HISTFILE
-# builtin fc -W "$HISTFILE"
